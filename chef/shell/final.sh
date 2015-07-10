f3af16a5c3b171ba1220ecc7cf3f51545a433b54
Adrien Revel <adrien@abrege.net>
2015-07-10 14:15:08 +0200

    Prepare custom VDD.

--- chef/shell/final.sh
+++ chef/shell/final.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
+
 VDD_IP=$(echo "$1")
 
 echo "============================================================="
 echo "Install finished! Visit http://$VDD_IP in your browser." 
+echo "============================================================="
+
+# Prepare needed commands
+GIT=`which git`
+CD=`which cd`
+SH=`which sh`
+
+# Go to www
+$CD /var/www
+
+# Clone git repository
+$GIT clone https://github.com/jabberwooki/pnf.git
+$CD pnf
+
+# Switch to develop branch
+$GIT checkout develop
+
+# Launch build script
+$SH scripts/build.sh
+
+echo "============================================================="
+echo "PNF BUILD OK"
 echo "============================================================="
