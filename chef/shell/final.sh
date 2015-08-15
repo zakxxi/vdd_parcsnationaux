4837e7cfa6622314f94891b46529cb27ef0ec595
Adrien Revel <adrien@abrege.net>
2015-08-15 18:10:21 +0200

    Update final.sh script to install pnf and cevennes sites.

--- chef/shell/final.sh
+++ chef/shell/final.sh
@@ -25,3 +25,17 @@ sh scripts/build.sh
 echo "============================================================="
 echo "PNF BUILD OK"
 echo "============================================================="
+
+drush @parcsnationaux si pnf -y
+
+echo "============================================================="
+echo "PNF INSTALL OK"
+echo "============================================================="
+
+cp -r /var/www/pnf/shared/template /var/www/pnf/shared/cevennes-parcnational.fr
+ln -s /var/www/pnf/shared/cevennes-parcnational.fr /var/www/pnf/www/sites/cevennes-parcnational.fr
+drush @cevennes si pnf -y
+
+echo "============================================================="
+echo "CEVENNES INSTALL OK"
+echo "============================================================="
